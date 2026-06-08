f95c8a020103dcaf811aa0b3ce1fc1ca51b5b983
Daniel Brai <danielbrai.dev@gmail.com>
2026-06-08 11:47:00 +0100

    chore(lib): Update paths

--- notifications/emitter/__init__.py
+++ notifications/emitter/__init__.py
@@ -1,11 +1,10 @@
-from .helpers import redis_emitter_from_client, redis_emitter_from_pool, in_memory_emitter
+from .helpers import redis_emitter_from_url, in_memory_emitter
 from .base import EventEmitter
 from .memory import InMemoryEventEmitter
 from .redis import RedisEventEmitter
 
 __all__ = [
-    "redis_emitter_from_client",
-    "redis_emitter_from_pool",
+    "redis_emitter_from_url",
     "in_memory_emitter",
     "EventEmitter",
     "InMemoryEventEmitter",
